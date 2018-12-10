1b6a605d76791201f1c67938738ab3c8fd541afd
Robin Peter <rpeter@Robins-MBPN.local>
2018-12-10 10:43:48 -0800

    Initial Commit

---
+++ camelcase.py
@@ -0,0 +1,14 @@
+#
+# https://www.codewars.com/kata/camelcase-method/train/python
+#
+
+
+def camel_case(string):
+    return ''.join([i for i in string.title() if i.isalpha()])
+
+
+print(camel_case("test case"), "TestCase")
+print(camel_case("camel case method"), "CamelCaseMethod")
+print(camel_case("say hello "), "SayHello")
+print(camel_case(" camel case word"), "CamelCaseWord")
+print(camel_case(""), "")
