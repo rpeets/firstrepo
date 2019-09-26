60dc51913a0736555f7e0d4c08271a211d017cd9
rpeter <robinboby@gmail.com>
2019-09-26 15:06:46 -0700

    Create earthquick.py
    -

---
+++ earthquick.py
@@ -0,0 +1,39 @@
+#
+#
+#
+import requests, json, time, datetime 
+from collections import namedtuple
+
+def readData():
+    #url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojsonn'
+    url = 'https://tinyurl.com/y34hprk5' 
+    try:
+        data = json.loads(requests.get(url).text)['features']
+    except requests.exceptions.RequestException as e:
+        print(e)
+        exit(1)
+    except json.decoder.JSONDecodeError as je:
+        print('Decoding JSON has failed', je)
+        exit(1)
+    return data
+
+
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
+def main():
+    #print(readconfig())
+    #print(json.dumps(readData(), indent=4))
+    newlist = sorted(readData(), key=lambda k: k['properties']['time'])
+    #print(newlist)
+    for i in newlist:
+        s = Struct(**i['properties'])
+        if ', CA' in s.place :
+            quaketime = time.strftime('%Y-%m-%dT%H:%M:%S+00:00',\
+                time.localtime(float(s.time)/1000))
+            print(quaketime,'|',s.place,'| Magnitude:',s.mag)
+
+if __name__ == '__main__':
+    main()
