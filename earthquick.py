9a15573be4f1e7b9de6044842200adf7b837882e
rpeter <robinboby@gmail.com>
2019-09-27 14:33:18 -0700

    Update earthquick.py

--- earthquick.py
+++ earthquick.py
@@ -5,17 +5,18 @@ import requests, json, time, datetime
 from collections import namedtuple
 
 def readData(url):
-    #url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-    #url = 'https://tinyurl.com/y34hprk5' 
     try:
         response = json.loads(requests.get(url).text)['features']
-    except requests.exceptions.RequestException as e:
-        print(e)
+        #response = json.loads(requests.get(url).text)
+        #print(json.dumps(response, indent=4))
+        #exit(0)
+        #response = json.loads(jsondata)['features']
+    except requests.exceptions.RequestException as reqerr:
+        print(reqerr)
         exit(1)
-    except json.decoder.JSONDecodeError as je:
-        print('Decoding JSON has failed', je)
+    except json.decoder.JSONDecodeError as jsonerr:
+        print('Decoding JSON has failed', jsonerr)
         exit(1)
-    #return response
     data = sorted(response, key=lambda k: k['properties']['time'])
     return data
 
@@ -25,16 +26,29 @@ class Struct:
         self.__dict__.update(entries)
 
 
+def utcTime(epochTime):
+    utc = time.strftime('%Y-%m-%dT%H:%M:%S+00:00',\
+                time.localtime(float(epochTime)/1000))
+    return utc
+
+
+def stateName(place):
+    return place.replace(', CA', ', California')
+
+
 def main():
+    #url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
     url = 'https://tinyurl.com/y34hprk5'
-    #print(json.dumps(readData(), indent=4))
+    
+    #print(json.dumps(readData(url), indent=4))
+    
     newlist = readData(url)
     for i in newlist:
         s = Struct(**i['properties'])
         if s.place.endswith(', CA') and s.type == 'earthquake':
-            quaketime = time.strftime('%Y-%m-%dT%H:%M:%S+00:00',\
-                time.localtime(float(s.time)/1000))
-            print(quaketime,'|',s.place,'| Magnitude:',s.mag)
+            #utctime = utcTime(s.time)
+            print(utcTime(s.time),'|',stateName(s.place),'| Magnitude:',s.mag)
+
 
 if __name__ == '__main__':
     main()
