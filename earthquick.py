5ecdc0ad57f3b22cb32ca7bccebd6032b09379f1
rpeter <robinboby@gmail.com>
2019-09-26 22:41:25 -0700

    Update earthquick.py
    -

--- earthquick.py
+++ earthquick.py
@@ -4,17 +4,19 @@
 import requests, json, time, datetime 
 from collections import namedtuple
 
-def readData():
-    #url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojsonn'
-    url = 'https://tinyurl.com/y34hprk5' 
+def readData(url):
+    #url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+    #url = 'https://tinyurl.com/y34hprk5' 
     try:
-        data = json.loads(requests.get(url).text)['features']
+        response = json.loads(requests.get(url).text)['features']
     except requests.exceptions.RequestException as e:
         print(e)
         exit(1)
     except json.decoder.JSONDecodeError as je:
         print('Decoding JSON has failed', je)
         exit(1)
+    #return response
+    data = sorted(response, key=lambda k: k['properties']['time'])
     return data
 
 
@@ -24,13 +26,11 @@ class Struct:
 
 
 def main():
-    #print(readconfig())
+    url = 'https://tinyurl.com/y34hprk5'
     #print(json.dumps(readData(), indent=4))
-    newlist = sorted(readData(), key=lambda k: k['properties']['time'])
-    #print(newlist)
+    newlist = readData(url)
     for i in newlist:
         s = Struct(**i['properties'])
-        #if ', CA' in s.place :
         if s.place.endswith(', CA') and s.type == 'earthquake':
             quaketime = time.strftime('%Y-%m-%dT%H:%M:%S+00:00',\
                 time.localtime(float(s.time)/1000))
